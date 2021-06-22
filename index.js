dae70de709f21a125074b804af9de1258a8476d5
haganWu <101731671@qq.com>
2021-06-22 17:21:09 +0800

    打包发布--配置应用名称版本号,使用android8.0可使用http

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ import 'react-native-gesture-handler';
 import App from './src';
 import {name as appName} from './app.json';
 
+if (!__DEV__) {
+  //非开发环境 清空日志打印
+  const emptyFunc = () => {};
+  global.console.info = emptyFunc;
+  global.console.log = emptyFunc;
+  global.console.warn = emptyFunc;
+  global.console.error = emptyFunc;
+}
+
 AppRegistry.registerComponent(appName, () => App);
